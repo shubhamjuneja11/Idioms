67f01b8e3d9af972ee166e1c542807c4c2734c23
Shubham <shubhamjuneja11@gmail.com>
2017-12-25 21:38:37 +0530

    json working

--- app/src/main/java/com/supergeekdeveloper/idioms/AlphabetActivity.java
+++ app/src/main/java/com/supergeekdeveloper/idioms/AlphabetActivity.java
@@ -9,16 +9,25 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.app.ActionBar;
+import android.util.Log;
 import android.view.Window;
 
 
 import com.supergeekdeveloper.idioms.adapters.TabsPagerAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AlphabetActivity extends FragmentActivity implements
         ActionBar.TabListener {
     private ViewPager viewPager;
     private TabsPagerAdapter mAdapter;
     private ActionBar actionBar;
+    String c="a";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,14 +43,17 @@ public class AlphabetActivity extends FragmentActivity implements
         for (int i = 0; i < 26; i++) {
             char ch=(char)(65+i);
             String s=ch+"";
-            actionBar.addTab(actionBar.newTab().setText(s).setTabListener(tabListener));
+            actionBar.addTab(actionBar.newTab().setText(s).setTabListener(this));
 
         }
-
+        loaddata(c);
     }
-    
+
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        String c=tab.getText()+"";
+        c=c.toLowerCase();
+        loaddata(c);
 
     }
 
@@ -54,4 +66,36 @@ public class AlphabetActivity extends FragmentActivity implements
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
 
     }
+    public void loaddata(String s){
+        s=s+".json";
+        Log.e("abc",s);
+        try {
+            JSONObject object=new JSONObject(loadJSONFromAsset(s));
+            JSONArray array=object.getJSONArray("idiom");
+            for (int i=0;i<array.length();i++){
+                JSONObject idiom=array.getJSONObject(i);
+                String name=idiom.getString("name");
+                Log.e("abc",name);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public String loadJSONFromAsset(String filename) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+
 }
