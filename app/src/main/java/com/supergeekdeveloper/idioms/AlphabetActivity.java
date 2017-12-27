6ee8ec301ce61f65e47eb360452a9ddc873ad675
Shubham <shubhamjuneja11@gmail.com>
2017-12-28 01:21:31 +0530

    changes

--- app/src/main/java/com/supergeekdeveloper/idioms/AlphabetActivity.java
+++ app/src/main/java/com/supergeekdeveloper/idioms/AlphabetActivity.java
@@ -4,18 +4,20 @@ package com.supergeekdeveloper.idioms;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 
 
-import com.supergeekdeveloper.idioms.adapters.TabsPagerAdapter;
 import com.supergeekdeveloper.idioms.model.AlphabetModel;
 
 import org.json.JSONArray;
@@ -25,25 +27,33 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class AlphabetActivity extends FragmentActivity implements
-        ActionBar.TabListener {
-    private ViewPager viewPager;
-    private TabsPagerAdapter mAdapter;
+        ActionBar.TabListener,TextToSpeech.OnInitListener,AlphabetAdapter.MyTextoSpeech {
+
     private ActionBar actionBar;
     AlphabetAdapter adapter;
     RecyclerView recyclerView;
     ArrayList<AlphabetModel> al;
-    AlphaInter af;
+    JSONObject object;
+    JSONObject object1;
+    private TextToSpeech tts;
+    String c = "a";
 
-    String c="a";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_alphabet);
-        viewPager =  findViewById(R.id.pager);
         actionBar = getActionBar();
+        tts = new TextToSpeech(this,this);
+        try {
+            object = new JSONObject(loadJSONFromAsset("allidioms.json"));
+            object1 = object.getJSONObject("idioms");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         /*mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
         af=mAdapter.getAlphabetFragment();
         if(af==null)
@@ -54,32 +64,53 @@ public class AlphabetActivity extends FragmentActivity implements
         actionBar.setHomeButtonEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         for (int i = 0; i < 26; i++) {
-            char ch=(char)(65+i);
-            String s=ch+"";
+            char ch = (char) (65 + i);
+            String s = ch + "";
             actionBar.addTab(actionBar.newTab().setText(s).setTabListener(this));
 
         }
 
 
-        al=new ArrayList<>();
+        al = new ArrayList<>();
 
-        adapter=new AlphabetAdapter(this,al);
-        recyclerView=findViewById(R.id.recycler);
+        adapter = new AlphabetAdapter(this, al);
+        recyclerView = findViewById(R.id.recycler);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(adapter);
 
-        loaddata(c);
+        nowloaddata("a");
     }
+public void nowloaddata(String a){
+    try {
 
+        al.clear();
+        JSONArray array = object1.getJSONArray(a);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject idiom=array.getJSONObject(i);
+            String name = idiom.getString("name");
+            if (name.length() >= 2)
+                name = name.substring(0, 1).toUpperCase() + name.substring(1);
+            JSONArray meaningarray = idiom.getJSONArray("meaning");
+            String meaning = meaningarray.getString(0);
+            int id = idiom.getInt("id");
+            al.add(new AlphabetModel(id, name, meaning));
+        }
+        adapter.notifyDataSetChanged();
+
+    } catch (JSONException e) {
+        Log.e("abcd", "exc");
+        e.printStackTrace();
+    }
+}
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        String c=tab.getText()+"";
-        c=c.toLowerCase();
+        String c = tab.getText() + "";
+        c = c.toLowerCase();
         //af.clear();
-        if(al!=null)
-        loaddata(c);
+        if (al != null)
+            nowloaddata(c);
 
     }
 
@@ -92,22 +123,23 @@ public class AlphabetActivity extends FragmentActivity implements
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
 
     }
-    public void loaddata(String s){
-        s=s+".json";
-        Log.e("abc",s);
-        try {  al.clear();
-           // af.clear();
-            JSONObject object=new JSONObject(loadJSONFromAsset(s));
-            JSONArray array=object.getJSONArray("idiom");
-            for (int i=0;i<array.length();i++){
-                JSONObject idiom=array.getJSONObject(i);
-                String name=idiom.getString("name");
-                if(name.length()>=2)
-                name=name.substring(0,1).toUpperCase()+name.substring(1);
-                JSONArray meaningarray=idiom.getJSONArray("meaning");
-                String meaning=meaningarray.getString(0);
-                int id=idiom.getInt("id");
-               al.add(new AlphabetModel(id,name,meaning));
+
+    public void loaddata(String s) {
+        s = s + ".json";
+        try {
+            al.clear();
+            // af.clear();
+            JSONObject object = new JSONObject(loadJSONFromAsset(s));
+            JSONArray array = object.getJSONArray("idiom");
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject idiom = array.getJSONObject(i);
+                String name = idiom.getString("name");
+                if (name.length() >= 2)
+                    name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                JSONArray meaningarray = idiom.getJSONArray("meaning");
+                String meaning = meaningarray.getString(0);
+                int id = idiom.getInt("id");
+                al.add(new AlphabetModel(id, name, meaning));
 
             }
             adapter.notifyDataSetChanged();
@@ -116,6 +148,7 @@ public class AlphabetActivity extends FragmentActivity implements
             e.printStackTrace();
         }
     }
+
     public String loadJSONFromAsset(String filename) {
         String json = null;
         try {
@@ -132,5 +165,23 @@ public class AlphabetActivity extends FragmentActivity implements
         return json;
     }
 
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
 
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            }
+
+        } else {
+
+        }
+    }
+
+    @Override
+    public void speak(String s) {
+        tts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+    }
 }
--- app/src/main/java/com/supergeekdeveloper/idioms/MainActivity.java
+++ app/src/main/java/com/supergeekdeveloper/idioms/MainActivity.java
@@ -1,9 +1,11 @@
 package com.supergeekdeveloper.idioms;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void openAlphabets(View view){
+        Intent intent=new Intent(this,AlphabetActivity.class);
+        startActivity(intent);
+    }
+    public void openCategories(View view){
+        Intent intent=new Intent(this,CategoryActivity.class);
+        startActivity(intent);
+    }
+    public void openCards(View view){
+        Intent intent=new Intent(this,AlphabetActivity.class);
 
     }
 }
